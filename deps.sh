4f372cb748953a091be2764cc1e6201117f8be96
Luuk Berenschot <luukberenschot@icloud.com>
2023-03-26 10:14:54 +0200

    Updated dependencies
    Ensure the dependencies of binwalk are installed correctly.

--- deps.sh
+++ deps.sh
@@ -4,6 +4,7 @@ pip install web3
 pip install pytz
 pip install colorama
 pip install pyfiglet
+yes | sudo apt-get install mtd-utils gzip bzip2 tar arj lhasa p7zip p7zip-full cabextract cramfsswap squashfs-tools sleuthkit default-jdk lzop srecord
 git clone https://github.com/ReFirmLabs/binwalk.git
 cd binwalk
 yes | sudo ./deps.sh
